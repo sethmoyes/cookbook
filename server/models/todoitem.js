4e6e3316ff342e9ba3e4a484bda57ade58aee418
smoyes <smoyes@myrounding.com>
2018-03-12 22:27:36 -0600

    finished

--- server/models/todoitem.js
+++ server/models/todoitem.js
@@ -18,4 +18,52 @@ module.exports = (sequelize, DataTypes) => {
   };
 
   return TodoItem;
+update(req, res) {
+  return TodoItem
+    .find({
+        where: {
+          id: req.params.todoItemId,
+          todoId: req.params.todoId,
+        },
+      })
+    .then(todoItem => {
+      if (!todoItem) {
+        return res.status(404).send({
+          message: 'TodoItem Not Found',
+        });
+      }
+
+      return todoItem
+        .update({
+          content: req.body.content || todoItem.content,
+          complete: req.body.complete || todoItem.complete,
+        })
+        .then(updatedTodoItem => res.status(200).send(updatedTodoItem))
+        .catch(error => res.status(400).send(error));
+    })
+    .catch(error => res.status(400).send(error));
+},
+
+destroy(req, res) {
+  return TodoItem
+    .find({
+        where: {
+          id: req.params.todoItemId,
+          todoId: req.params.todoId,
+        },
+      })
+    .then(todoItem => {
+      if (!todoItem) {
+        return res.status(404).send({
+          message: 'TodoItem Not Found',
+        });
+      }
+
+      return todoItem
+        .destroy()
+        .then(() => res.status(204).send())
+        .catch(error => res.status(400).send(error));
+    })
+    .catch(error => res.status(400).send(error));
+},
 };
